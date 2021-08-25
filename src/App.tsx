2bf38d6013f1975ddbe48bce4e5da96d883c6c97
Stanley <stanleygarbo@gmail.com>
2021-08-25 08:24:06 +0800

    added router memory

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,42 @@ import HomePage from "./pages/HomePage";
 import AddScholar from "./pages/AddScholar";
 import Search from "./pages/Search";
 import ScholarPage from "./pages/ScholarPage";
-import ScrollToTop from "./components/ScrollToTop";
 import About from "./pages/About";
 import Donate from "./pages/Donate";
 import Settings from "./pages/Settings";
 import SettingsBulkImport from "./pages/SettingsBulkImport";
+import ScrollMemory from "react-router-scroll-memory";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-        <ScrollToTop />
         <Layout>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/add-scholar" component={AddScholar} />
-            <Route exact path="/scholar/:ronin" component={ScholarPage} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/donate" component={Donate} />
-            <Route
-              path="/settings"
-              render={({ match: { path } }) => (
-                <>
-                  <Route path={`${path}/`} component={Settings} exact />
-                  <Route
-                    path={`${path}/bulk-import`}
-                    component={SettingsBulkImport}
-                    exact
-                  />
-                  {/* <Route path={`${path}/users`} component={UserPage} /> */}
-                </>
-              )}
-            />
-          </Switch>
+          <>
+            <ScrollMemory />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/add-scholar" component={AddScholar} />
+              <Route exact path="/scholar/:ronin" component={ScholarPage} />
+              <Route exact path="/search" component={Search} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/donate" component={Donate} />
+              <Route
+                path="/settings"
+                render={({ match: { path } }) => (
+                  <>
+                    <Route path={`${path}/`} component={Settings} exact />
+                    <Route
+                      path={`${path}/bulk-import`}
+                      component={SettingsBulkImport}
+                      exact
+                    />
+                    {/* <Route path={`${path}/users`} component={UserPage} /> */}
+                  </>
+                )}
+              />
+            </Switch>
+          </>
         </Layout>
       </Router>
     </div>
