d9e0d5cecdff7a76d3449af5cc285e353e6e6d2c
Stanley <stanleygarbo@gmail.com>
2021-09-10 16:45:22 +0800

    added fb link to message

--- src/templates/HomeSection1.tsx
+++ src/templates/HomeSection1.tsx
@@ -85,7 +85,16 @@ const HomeSection1: React.FC<{
         <span>Notice:</span> Please take the 'Today SLP' data with a grain of
         salt, for more accuracy look at the charts to see your scholars'
         earnings along with a particular date. Thanks so much for your
-        donations, may pang load at pang bayad nako sa servers :)
+        donations, may pang load at pang bayad nako sa servers :) Hit me up on
+        &nbsp;
+        <a
+          href="https://facebook.com/stanley.garbo29"
+          rel="noreferrer"
+          target="_blank"
+        >
+          FB messenger
+        </a>
+        &nbsp; if you have any questions.
         {/* <Link to="/announcement">learn more</Link>. */}
       </p>
       <div className="home-section1-wrapper">
