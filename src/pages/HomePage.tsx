aad9f560a7959c615ff287c13c8ec156d36d1f2b
Stanley <stanleygarbo@gmail.com>
2021-10-09 07:44:51 +0800

    added a notice card

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import HomeSection1 from "../templates/HomeSection1";
 import HomeSection2 from "../templates/HomeSection2";
 import AddScholarForm from "../components/AddScholarForm";
@@ -11,22 +11,76 @@ import {
 } from "../api/requests";
 import { Scholars } from "../interfaces/IResponseTypes";
 import { useUserPreferences } from "../contexts/userPreferences";
+import { useTheme } from "../contexts/themeContext";
+import { IColors } from "../interfaces/IColors";
 
-const Container = styled.div`
-  padding-bottom: 50px;
-
-  .ad {
-    padding: 0 10px;
-    margin-top: 20px;
+const Container = styled.div<{ colors: IColors }>`
+  ${({ colors }) => css`
+    padding-bottom: 50px;
     width: 100%;
-    display: flex;
-    justify-content: center;
-  }
+    .ad {
+      padding: 0 20px;
+      margin: 0 auto;
+      margin-top: 20px;
+      max-width: 1200px;
+      display: flex;
+      justify-content: center;
+      display: grid;
+      grid-template-columns: 1fr 728px;
+      gap: 10px;
+
+      .notice {
+        border: 1px dashed ${colors.textIntense + 30};
+        padding: 10px;
+        border-radius: 5px;
+
+        &__title {
+          color: ${colors.textNotSoIntense};
+          font-size: 16px;
+          font-weight: 500;
+        }
+        p {
+          color: ${colors.textIntense + 90};
+          font-size: 13px;
+          margin-top: 3px;
+        }
+      }
+    }
+
+    @media (max-width: 1152px) {
+      .ad {
+        .notice {
+          &__title {
+            font-size: 14px;
+          }
+          p {
+            font-size: 11px;
+          }
+        }
+      }
+    }
+    @media (max-width: 1091px) {
+      .ad {
+        display: flex;
+        justify-content: center;
+
+        .notice {
+          display: none;
+        }
+      }
+    }
+    @media (max-width: 493px) {
+      .ad {
+        padding: 0 10px;
+      }
+    }
+  `}
 `;
 
 const HomePage = () => {
   const { scholars } = useScholars();
   const { currency } = useUserPreferences();
+  const { colors } = useTheme();
 
   const ids = scholars?.map((i) => i.ronin.replace("ronin:", "0x"));
 
@@ -65,7 +119,7 @@ const HomePage = () => {
   );
 
   return (
-    <Container>
+    <Container colors={colors}>
       <div className="section1">
         <HomeSection1
           scholarsQuery={scholarsQuery}
@@ -73,6 +127,13 @@ const HomePage = () => {
         />
 
         <div className="ad">
+          <div className="notice">
+            <div className="notice__title">Notice</div>
+            <p>
+              For the daily SLP to be accurate, it is recommended for your
+              players to play 1hr after reset.
+            </p>
+          </div>
           <div className="coinzilla" data-zone="C-8615a9fd1747a828"></div>
         </div>
         <AddScholarForm />
