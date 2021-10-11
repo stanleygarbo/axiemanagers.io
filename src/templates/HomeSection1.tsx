319b1f4d8931bb02d5149a210530a15d727060c8
Stanley <stanleygarbo@gmail.com>
2021-10-11 13:08:20 +0800

    termporarily commented out message

--- src/templates/HomeSection1.tsx
+++ src/templates/HomeSection1.tsx
@@ -85,12 +85,12 @@ const HomeSection1: React.FC<{
 
   return (
     <Container colors={colors}>
-      <p>
+      {/* <p>
         <span>Update:</span> You can now view your scholars' daily SLP reports
         from the past 14 days, just tap/click your scholar to view. Side note:
         if you dont like the randomly selected color of your scholar you can
         change it.
-      </p>
+      </p> */}
       <div className="home-section1-wrapper">
         <div className="home-section1-wrapper__grid">
           <div className="home-section1-wrapper__grid__total-farmed box home-section1-wrapper__grid__item">
