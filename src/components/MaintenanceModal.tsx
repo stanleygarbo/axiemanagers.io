0a31db8c822f038af9ef5d705cc6df3a491ff477
Stanley <stanleygarbo@gmail.com>
2021-10-15 14:06:36 +0800

    increased z index

--- src/components/MaintenanceModal.tsx
+++ src/components/MaintenanceModal.tsx
@@ -30,7 +30,7 @@ const Container = styled.div<{ colors: IColors }>`
     left: 0;
     width: 100%;
     height: 100vh;
-    z-index: 100;
+    z-index: 1000;
     display: flex;
     align-items: center;
     justify-content: center;
