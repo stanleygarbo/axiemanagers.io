6ca67fd3c84ca16d009dba700f19ba46954708e8
Stanley <stanleygarbo@gmail.com>
2021-11-10 12:18:17 +0800

    changed modal

--- src/templates/HomeSection1.tsx
+++ src/templates/HomeSection1.tsx
@@ -85,7 +85,8 @@ const HomeSection1: React.FC<{
     <Container colors={colors}>
       <p>
         <span>NOTICE</span> Today SLP is currently undergoing maintenance, it
-        should work fine tomorrow.
+        should work fine tomorrow. We just upgraded the server, the time taken
+        to record everyone's total SLP will be reduced.
       </p>
       <div className="home-section1-wrapper">
         <div className="home-section1-wrapper__grid">
